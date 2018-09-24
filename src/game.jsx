a2aff6952d13ca08a4d16d8377683a8125110bf9
MrSlips <james.seabrook@brandworkz.com>
2018-09-24 11:21:18 +0100

    Correction from hero.position to hero.rotation.

--- src/game.jsx
+++ src/game.jsx
@@ -319,7 +319,7 @@ class Game extends Component {
             }
         }
 
-        hero.position.z = controllerState.rotate + 1.5;
+        hero.rotation.z = controllerState.rotate + 1.5;
 
         for (var vertexIndex = 0; vertexIndex < hero.geometry.vertices.length; vertexIndex++){      
             var localVertex = hero.geometry.vertices[vertexIndex].clone();
