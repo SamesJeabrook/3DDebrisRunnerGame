b80a2a974147595f80abe1e98b3cb50898d79739
MrSlips <james.seabrook@brandworkz.com>
2018-09-25 17:47:55 +0100

    Added the controller disconnected message

---
+++ src/controllerDisconnected.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// styles
+
+import './styles/ControllerDisconnected.css';
+
+class ControllerDisconnected extends Component {
+    render(){
+        return(
+            <div id="ControllerDisconnectedContainer">
+                <h3>Controller Disconnected</h3>
+                <span className="vertical_align"></span>
+            </div>
+        )
+    }
+}
+export default ControllerDisconnected;
